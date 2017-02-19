29d7aff5aff62a874d836ea466f7227c0d78ba2c
ggauze <gringauze@gmail.com>
2017-02-19 00:56:25 -0800

    Initial Commit

---
+++ questions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  questions
+//
+//  Created by Sergei Gringauze on 2/19/17.
+//  Copyright © 2017 Sergei Gringauze. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
